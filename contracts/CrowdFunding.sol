5ce6b883de9a84961704f0a6faa3c4947c4f801d
Ramesh Edirisinghe <e2145434@bit.uom.lk>
2026-01-14 16:32:54 +0530

    fix: correct return type in getCampaigns function and update default network to Sepolia in configuration

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -35,6 +35,7 @@ contract CrowdFunding {
         numberOfCampaigns++;
 
         return numberOfCampaigns - 1;
+     
      }
 
     function donateToCampaign(uint256 _id) public payable {
@@ -55,7 +56,7 @@ contract CrowdFunding {
         return (campaigns[_id].donators, campaigns[_id].donations);
     }
 
-    function getCampaigns() public view returns {Campaign[] memory} {
+    function getCampaigns() public view returns (Campaign[] memory) {
         Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
 
         for(uint i = 0; i < numberOfCampaigns; i++) {
